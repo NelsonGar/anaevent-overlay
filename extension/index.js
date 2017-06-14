38c6e8eca2085327e1157d9b56845019e0f4e250
NelsonGar <garcia.nelson.francisco@gmail.com>
2017-06-14 14:52:07 +0200

    Only things i know

--- extension/index.js
+++ extension/index.js
@@ -29,44 +29,9 @@ module.exports = function (nodecg) {
 	}
 
 	try {
-		require('./total')(nodecg);
-	} catch (e) {
-		nodecg.log.error('Failed to load total lib:', e.stack);
-		process.exit(1);
-	}
-
-	try {
-		require('./donations')(nodecg);
-	} catch (e) {
-		nodecg.log.error('Failed to load donations lib:', e.stack);
-		process.exit(1);
-	}
-
-	try {
-		require('./bids')(nodecg);
-	} catch (e) {
-		nodecg.log.error('Failed to load bids lib:', e.stack);
-		process.exit(1);
-	}
-
-	try {
-		require('./completed-challenges')(nodecg);
-	} catch (e) {
-		nodecg.log.error('Failed to load completed-challenges lib:', e.stack);
-		process.exit(1);
-	}
-
-	try {
 		require('./nowplaying')(nodecg);
 	} catch (e) {
 		nodecg.log.error('Failed to load "now playing" lib:', e.stack);
-		process.exit(1);
-	}
-
-	try {
-		require('./dotafortress')(nodecg);
-	} catch (e) {
-		nodecg.log.error('Failed to load "dotafortress" lib:', e.stack);
 		process.exit(1);
 	}
 
@@ -78,23 +43,9 @@ module.exports = function (nodecg) {
 	}
 
 	try {
-		require('./x32')(nodecg);
-	} catch (e) {
-		nodecg.log.error('Failed to load "x32" lib:', e.stack);
-		process.exit(1);
-	}
-
-	try {
 		require('./countdown')(nodecg);
 	} catch (e) {
 		nodecg.log.error('Failed to load "countdown" lib:', e.stack);
-		process.exit(1);
-	}
-
-	try {
-		require('./nameplates')(nodecg);
-	} catch (e) {
-		nodecg.log.error('Failed to load "nameplates" lib:', e.stack);
 		process.exit(1);
 	}
 };
